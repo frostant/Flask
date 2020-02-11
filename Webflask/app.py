de2d426a03db5d5c836017db602d8df4d4da38d7
frostant <975125701@qq.com>
2020-02-11 10:11:09 +0800

    Chap05

--- Webflask/app.py
+++ Webflask/app.py
@@ -1,15 +1,58 @@
 from flask import Flask, escape, url_for, request, render_template
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+import os
+import click
 
-name = 'frostant'
-movies = [
-    {'title':'My love','year':'2000'},
-    {'title':'My frost','year':'2001'},
-    {'title':'My dream','year':'2002'},
-    {'title':'My her','year':'2003'},
-]
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 这个不是很理解
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(10))
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop')
+def initdb(drop):
+    if(drop):
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database')
+
+@app.cli.command()
+def forge():
+    """fake data"""
+    db.create_all()
+    name = 'frostant'
+    movies = [
+        {'title':'My love','year':'2000'},
+        {'title':'My frost','year':'2001'},
+        {'title':'My dream','year':'2002'},
+        {'title':'My her','year':'2003'},
+    ]
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    
+    db.session.commit()
+    click.echo('Done')
+
+
+
+# 通过数据库加入，应该不需要这段了
 
 @app.route('/')
 def index():
-    return render_template('index.html',name=name,movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html',user=user,movies=movies)
 
